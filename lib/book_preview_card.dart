26cb8c84fc4543ac32173b4937350b47c55cb255
eddymango <94836715+eddymango@users.noreply.github.com>
2023-02-27 11:08:13 +0900

    github upload

--- lib/book_preview_card.dart
+++ lib/book_preview_card.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:firebase_core/firebase_core.dart';
 
 final List<int> numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
 
@@ -10,14 +11,25 @@ class BookPreviewCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Container(
-        height: 200,
-        color: Colors.deepPurple,
-        child: Center(
-          child: Text(child),
-        ),
-      ),
-    );
+        padding: const EdgeInsets.all(1.0),
+        child: Row(children: [
+          Flexible(
+            flex: 1,
+            child: Image.asset(
+              'assets/images/book1.jpg',
+              height: 50,
+              width: 50,
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Column(
+              children: [
+                Text("이름"),
+                Text("저자"),
+              ],
+            ),
+          )
+        ]));
   }
 }
